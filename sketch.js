cab0e01a658bb5f8df02a2c304cc1aa316354257
Elam School of Fine Arts <156630095+Elam-Fine-Arts-205@users.noreply.github.com>
2024-01-29 22:50:35 +1300

    Added files to the project

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let counter = 0;
+
+let countInterval = setInterval(makeRedSquare, 1000);
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function makeRedSquare(){
+  noStroke();
+  fill(255, 0, 0);
+  rect(random(0,300), random(0,300), 60, 60);
+  counter++;
+  if (counter > 5){
+    clearInterval(countInterval);
+    counter = 0;
+  }
+}
+
+function mousePressed() {
+  countInterval = setInterval(makeRedSquare, 1000);
+}
+
